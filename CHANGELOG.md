b211e15fe46c895cc5900aea43e28104d0a38128
greymag <greymag@gmail.com>
2020-07-20 16:32:07 +0300

    Added tests for some extension getters.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 * Example code.
 * Add pedantic analysis ruleset.
 * Fix analysis problems.
+* Added tests for some extension getters.
 * Fixed: `isExternalServiceError` returns `true` for every internal server error.
 
 ## [0.1.0+2] - 2020-07-14
---
+++ test/remote_error_code_test.dart
@@ -0,0 +1,39 @@
+import 'package:dio/dio.dart';
+import 'package:innim_remote_error/innim_remote_error.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('CommonErrorResultExtensionErrorCode', () {
+    group('isNotFound', () {
+      test('should return true if http status is 404', () {
+        final res = _dioError(statusCode: 404);
+
+        expect(res.isNotFound, true);
+      });
+
+      test('should return false if http status is not 404', () {
+        final res = _dioError(statusCode: 400);
+
+        expect(res.isNotFound, false);
+      });
+    });
+
+    group('isBadRequest', () {
+      test('should return true if http status is 400', () {
+        final res = _dioError(statusCode: 400);
+
+        expect(res.isBadRequest, true);
+      });
+
+      test('should return false if http status is not 400', () {
+        final res = _dioError(statusCode: 401);
+
+        expect(res.isBadRequest, false);
+      });
+    });
+  });
+}
+
+ErrorResult _dioError({int statusCode}) {
+  return Result.error(DioError(response: Response(statusCode: statusCode)));
+}
